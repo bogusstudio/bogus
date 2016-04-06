95c10f5258966247b5264ce7e32dde71a71f5624
jaygoss <jaygoss@gmail.com>
2016-04-06 03:02:24 -0400

    new

---
+++ script.js
@@ -0,0 +1,20 @@
+var src = '';
+var main = document.getElementsByClassName("main")[0];
+
+$.getJSON("data.json", function(data) {
+    var images = data.images;
+    getImage(images);
+});
+
+function getImage(images) {
+    var randomIndex = Math.floor(Math.random() * images.length);
+    src = images[randomIndex];
+    main.style.backgroundImage = "url('" + src + "')";
+    setTimeout(function() {
+        main.classList.remove('opacZero')
+    }, 500);
+}
+
+$(window).on('beforeunload', function() {
+    main.classList.add('opacZero')
+});
